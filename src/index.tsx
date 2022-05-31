5c2b1a915ce514e910215fcf5787e0fc33c8eb41
champix56 <desorbaix@free.fr>
2022-05-31 15:57:02 +0100

    export des interfaces et emptymeme

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 //import * as React from 'react'
 // import styles from './styles.module.css'
+import { DummyMeme, IImage, IMeme } from './interfaces/common'
 import MemeThumbnail from './ui/MemeThumbnail/MemeThumbnail'
 import MemeViewer, { IMemeViewerProps } from './ui/MemeViewer/MemeViewer'
 // interface Props
@@ -12,3 +13,6 @@ import MemeViewer, { IMemeViewerProps } from './ui/MemeViewer/MemeViewer'
 export const MemeSVGViewer = MemeViewer
 export const MemeSVGThumbnail = MemeThumbnail
 export default IMemeViewerProps
+export interface MemeInterface extends IMeme {}
+export interface ImageInterface extends IImage {}
+export const emptyMeme = DummyMeme
